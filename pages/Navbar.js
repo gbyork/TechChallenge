70cca9bdbf8904f351317f32ad125b83c0a73dcd
Garrett <gbyork18@gmail.com>
2022-04-01 00:13:16 -0500

    updates

---
+++ pages/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (  
+        <nav>
+
+            <div className="logo">
+            <img src="/Logo.png" />
+            <h1>Midwestern</h1>
+            </div>
+            <a>Contact</a>
+
+        </nav>
+    );
+}
+ 
+export default Navbar;
