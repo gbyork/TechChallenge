3ec59b9ab944d3c636f1fe7133188beffd497568
Garrett <gbyork18@gmail.com>
2022-04-01 23:26:49 -0500

    edits

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 const Contact = () => {
     return (  
-        <div>
-        <div>
+        <div className="clearfix">
+        <div className="box" style="background-color:#bbb"></div>
         <img src="/Logo.png" />
+        <h1>Heading One</h1>
+        <p>lorem</p>
+        
         <div className="container2">
-
+        
         <div><Link href ="/"><a>Home</a></Link></div>
         
         </div> 
@@ -15,7 +18,7 @@ const Contact = () => {
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         </div>
-        </div>
+       
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -18,18 +18,18 @@ export default function Home() {
     <div className="grid-container">
         <div><img src="/Talkie.png" width={72} height={75}/>
         <h1>Heading 1</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Learn More</button>
+        <p style={{color:'grey'}}>ut labore et dolore magna aliqua. Ut enim ad minim veniam ullamco laboris nisi ut aliquip ex ea commodo consequat.,consectetur adipiscing elit consequat quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui</p>
+        <button style={{background:'gold'}}>Learn More</button>
         </div>
         <div> <img src="/Rabbit.png" width={72} height={75}/>
         <h1>Heading 1</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Learn More</button>
+        <p style={{color:'grey'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit consequat.ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button style={{background:'gold'}}>Learn More</button>
         </div>
         <div> <img src="/Shield.png" width={72} height={75}/>
         <h1>Heading 1</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Learn More</button>
+        <p style={{color:'grey'}}>ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button style={{background:'gold'}}>Learn More</button>
         </div>
     </div>
 
